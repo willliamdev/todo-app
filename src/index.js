6347024d158ec84dfb54f4ee3e73b16b92a6ffb4
William almeida <willliam.dev@gmail.com>
2022-05-16 01:59:08 -0300

    Checking if exists users account

--- src/index.js
+++ src/index.js
@@ -11,8 +11,18 @@ app.use(express.json());
 
 const users = [];
 
+// Mieddleware
+
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    return response.status(400).json({ error: 'User not found!' })
+  }
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
