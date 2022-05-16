98ab0f1fb4a0c075f42e44192a2b7c8e94dc7702
William almeida <willliam.dev@gmail.com>
2022-05-16 02:30:09 -0300

    route: post/todos  :white_check_mark:

--- src/index.js
+++ src/index.js
@@ -54,7 +54,21 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request
+  const { title, deadline } = request.body
+
+  const todo = {
+    id: uuidv4(),
+    title: title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(todo)
+
+  return response.status(201).json(todo)
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
